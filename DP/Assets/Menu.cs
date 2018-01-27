9adb218abcbcb30d4ce53c171b3401c54b794828
charles33u <anne-laure.charles1@uqac.ca>
2018-01-27 11:27:55 -0500

    first commit

---
+++ DP/Assets/Menu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject menu;
+    public GameObject generic;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void Hi()
+    {
+        Debug.Log("hi");
+    }
+
+    //Pour switcher entre GroupeMenu et Groupe Generic
+    public void GoMenu()
+    {
+        generic.SetActive(false);
+        menu.SetActive(true);
+    }
+
+    //Pour switcher entre Groupe Generic et GroupeMenu
+    public void GoGeneric()
+    {
+        generic.SetActive(true);
+        menu.SetActive(false);
+    }
+}
