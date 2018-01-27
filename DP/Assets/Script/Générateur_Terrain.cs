af6eefeb3b14f097fc50d4db50444c1646d66da5
Erwan-ROMANZIN <erwan.guy.r@gmail.com>
2018-01-27 17:52:44 -0500

    correction map en Y

--- DP/Assets/Script/Générateur_Terrain.cs
+++ DP/Assets/Script/Générateur_Terrain.cs
@@ -10,12 +10,12 @@ public class Générateur_Terrain : MonoBehaviour {
 		Debug.Log ("hi");
 		string pathTerrain1 = "Prefabs/Terrain"+Random.Range (1, 20);
 		string pathTerrain2 = "Prefabs/Terrain"+Random.Range (1, 20);
-		string pathTerrain3 = "Prefabs/Terrain"+Random.Range (1, 20);
-		Debug.Log (pathTerrain1 + " " + pathTerrain2 + " " + pathTerrain3);
+	//	string pathTerrain3 = "Prefabs/Terrain"+Random.Range (1, 20);
+		//Debug.Log (pathTerrain1 + " " + pathTerrain2 + " " + pathTerrain3);
 
 		GameObject g1 = Instantiate(Resources.Load(pathTerrain1), Vector2.zero, Quaternion.identity) as GameObject;
 		GameObject g2 = Instantiate(Resources.Load(pathTerrain2), new Vector2(g1.transform.position.x-26, g1.transform.position.y), Quaternion.identity) as GameObject;
-		GameObject g3 = Instantiate(Resources.Load(pathTerrain3), new Vector2(g1.transform.position.x+26, g1.transform.position.y), Quaternion.identity) as GameObject;
+		GameObject g3 = Instantiate(Resources.Load("Prefabs/Terrain20"), new Vector2(g1.transform.position.x+26, g1.transform.position.y), Quaternion.identity) as GameObject;
 	}
 	
 	// Update is called once per frame
