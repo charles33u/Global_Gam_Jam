e283d478ad5a3fa30574d7ed2789ca65612f3747
Erwan-ROMANZIN <erwan.guy.r@gmail.com>
2018-01-27 18:40:42 -0500

    move camera

---
+++ DP/Assets/Script/Generateur_Terrain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generateur_Terrain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("hi");
+		string pathTerrain1 = "Prefabs/Terrain"+Random.Range (1, 20);
+		string pathTerrain2 = "Prefabs/Terrain"+Random.Range (1, 20);
+		string pathTerrain3 = "Prefabs/Terrain"+Random.Range (1, 20);
+		//Debug.Log (pathTerrain1 + " " + pathTerrain2 + " " + pathTerrain3);
+
+		GameObject g1 = Instantiate(Resources.Load(pathTerrain1), Vector2.zero, Quaternion.identity) as GameObject;
+		GameObject g2 = Instantiate(Resources.Load(pathTerrain2), new Vector2(g1.transform.position.x-26, g1.transform.position.y), Quaternion.identity) as GameObject;
+		GameObject g3 = Instantiate(Resources.Load(pathTerrain3), new Vector2(g1.transform.position.x+26, g1.transform.position.y), Quaternion.identity) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
